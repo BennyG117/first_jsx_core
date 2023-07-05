265b21bc856ba9ebfd86d7f51f9f621e3efa5517
BennyG117 <bengomezp@gmail.com>
2023-07-05 14:26:00 -0700

    Initial Commit - First JSX Core

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <h1>
+        Hello Dojo!
+      </h1>
+      <h2>
+        Things I need to do:
+      </h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +29,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+    </>
   );
 }
 
